d1734af90e14c98d0e1b850350603735985af37c
sphan <santhai.phan@gmail.com>
2014-09-20 22:52:24 +1000

    Initial code commit

---
+++ DontBeLate/desktop/src/com/comp4920/dbl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.comp4920.dbl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.comp4920.dbl.DBL;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DBL(), config);
+	}
+}
