0fe3096a47f0d867a8ebdb10be04749baad1b331
sphan <santhai.phan@gmail.com>
2014-09-20 22:51:23 +1000

    Initial code commit

---
+++ DontBeLate/ios/src/com/comp4920/dbl/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.comp4920.dbl;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.comp4920.dbl.DBL;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DBL(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
