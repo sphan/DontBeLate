63aba3cf9a1c6595ef0cbcd250dbce16bc0bc9a7
sphan <santhai.phan@gmail.com>
2014-09-20 22:52:04 +1000

    Initial code commit.

---
+++ DontBeLate/android/src/com/comp4920/dbl/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.comp4920.dbl.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.comp4920.dbl.DBL;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DBL(), config);
+	}
+}
