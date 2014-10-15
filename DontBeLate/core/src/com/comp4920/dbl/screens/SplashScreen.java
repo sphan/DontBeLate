52d8f47036a489cabaf618113d151d555cde7fe5
Lina Wei <linawei_39@hotmail.com>
2014-10-15 23:41:26 +1100

    change position of buttons

--- DontBeLate/core/src/com/comp4920/dbl/screens/SplashScreen.java
+++ DontBeLate/core/src/com/comp4920/dbl/screens/SplashScreen.java
@@ -58,8 +58,7 @@ public class SplashScreen implements Screen {
 	@Override
 	public void show() {
 		stage.addActor(startButton);
-		stage.addActor(quitButton);
-		//System.out.println("width is " + width + " height is " + height); 
+		stage.addActor(quitButton); 
 		startButton.setPosition(2*width/27, 13*height/54);
 		quitButton.setPosition(2*width/27, 5*height/54);
 		Gdx.input.setInputProcessor(stage);
