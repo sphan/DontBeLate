6517a9e06cd32de3fe97582df81c69bdd0f79ed2
sphan <santhai.phan@gmail.com>
2014-10-19 00:58:34 +1100

    Fixed bug in playing count down sound.

--- DontBeLate/core/src/com/comp4920/dbl/gameworld/GameInterfaceRenderer.java
+++ DontBeLate/core/src/com/comp4920/dbl/gameworld/GameInterfaceRenderer.java
@@ -165,7 +165,9 @@ public class GameInterfaceRenderer {
 			getBitMapFont().draw(batch, timeLabel, posRemainingTimeLabX, posRemainingTimeLabY);
 		}
 		
-		if (myWorld.isRunning() && (runTime % 12 < 0.02 || runTime % 13 < 0.02 || runTime % 14 < 0.02)) {
+		if (myWorld.isRunning() &&
+			timeLeft <= 3 &&
+			(runTime % 12 < 0.02 || runTime % 13 < 0.02 || runTime % 14 < 0.02)) {
 			Gdx.app.log("runTime", String.valueOf(runTime));
 			AssetLoader.countDownSound.play(1.0f);
 		}
