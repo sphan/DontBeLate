8cb3231c46054afe48c8a426295316923df4cdee
double-pHelix <led_python@hotmail.com>
2014-10-18 18:17:28 +1100

    reset difficulty at restart.

--- DontBeLate/core/src/com/comp4920/dbl/gameworld/GameWorld.java
+++ DontBeLate/core/src/com/comp4920/dbl/gameworld/GameWorld.java
@@ -44,7 +44,7 @@ public class GameWorld {
 	private GameState state;
 
 	public GameWorld(int midPointX) {
-
+		maxNumCars = 1;
 		stopped = false;
 		lastCarTime = 0;
 		bus = new Bus(midPointX-Bus.BUS_WIDTH/2, Bus.BUS_START_Y, Bus.BUS_WIDTH, Bus.BUS_HEIGHT);
